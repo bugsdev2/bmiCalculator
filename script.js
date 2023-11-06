0fa053abd8a0d7fc15257b3f8a3b6f14f9879c81
BugsLaptop <msherlock246@gmail.com>
2023-11-06 21:50:13 +0530

    Added toggle switch which changes height to foot and inches from centimeters

--- script.js
+++ script.js
@@ -1,35 +1,80 @@
 const age = document.getElementById('age');
 const height = document.getElementById('height');
 const weight = document.getElementById('weight');
+const foot = document.getElementById('foot');
+const inches = document.getElementById('inches');
+
+const centimetres = document.getElementById('centimetres');
+const footInches = document.getElementById('footInches-container');
 
 const button = document.querySelector('button');
 const messageContainer = document.getElementById('message');
-button.addEventListener('click', calculate)
+const toggle = document.getElementById('toggle');
+
+
+if(toggle.checked){
+	footInches.style.display = "block";
+	centimetres.style.display = "none";
+	document.getElementById('p_centi').style.opacity = 0.5;
+	document.getElementById('p_foot').style.opacity = 1;
+} else {
+	centimetres.style.display = "block";
+	footInches.style.display = "none";
+	document.getElementById('p_foot').style.opacity = 0.5;
+	document.getElementById('p_centi').style.opacity = 1;
+}
+
+toggle.addEventListener('change', toggleHeight)
+function toggleHeight(){
+	if(toggle.checked){
+	footInches.style.display = "block";
+	centimetres.style.display = "none";
+	document.getElementById('p_centi').style.opacity = 0.5;
+	document.getElementById('p_foot').style.opacity = 1;
+	} else {
+	centimetres.style.display = "block";
+	footInches.style.display = "none";
+	document.getElementById('p_foot').style.opacity = 0.5;
+	document.getElementById('p_centi').style.opacity = 1;
+	}
+}
+
+button.addEventListener('click', calculate);
 
 function calculate(){
-	if(isNaN(age.value) == isNaN(height.value) == isNaN(weight.value) == true) message.textContent == "Recheck what you've typed";
-	if(age.value != ''){
+	if(toggle.checked){
+		let totalInches = parseInt(foot.value*12) + parseInt(inches.value);
+		let weightInPounds = parseInt(weight.value) * 2.204622621849;
+		let result = (Math.round(weightInPounds / ((totalInches**2))*100)/100)*703;
+		message.innerHTML = `Your BMI is ${result} <br> You are ${changeColor(result)}`;
+		changeColor(result);
+	} else {
 		if(height.value != ''){
-			if(weight.value != ''){
-				let result = Math.round(weight.value / ((height.value/100)**2));
-				message.innerText = `Your BMI is ${result}`;
-				if(result<18.5){
-					message.style.color = "#FFC107";
-				} else if(result>=18.5 && result<25){
-					message.style.color="#198754";
-				} else if(result>=25 && result<30){
-					message.style.color = "#FF8C00";
-				} else {
-					message.style.color = "#DC3545";
-				}
+		if(weight.value != ''){
+			let result = Math.round((weight.value / ((height.value/100)**2)*100))/100;
+			message.innerHTML = `Your BMI is ${result} <br> You are ${changeColor(result)}`;
+			changeColor(result);
 			} else {
 				message.textContent = 'Enter Weight'
 			}
 		} else {
 			message.textContent = 'Enter Height'
 		}
-	} else {
-		message.textContent = 'Enter Age';
 	}
-	
+}
+
+function changeColor(result){
+	if(result<18.5){
+			message.style.color = "#FFC107";
+			return 'Underweight';
+		} else if(result>=18.5 && result<25){
+			message.style.color="#198754";
+			return 'Normal';
+		} else if(result>=25 && result<30){
+			message.style.color = "#FF8C00";
+			return 'Overweight';
+		} else {
+			message.style.color = "#DC3545";
+			return 'Obese'
+		}
 }
